0cfe157931b31941a75bb70d680f68892800120e
Agnieszka Draganczyk <Agnieszka.127@interia.pl>
2024-01-20 11:39:14 +0100

    first react component

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
 import './App.css'
+import TaskCard from './task-card'
+
 
 function App() {
   const title = 'Do Market Research'
   const id = 'BUS-1'
   const points = 5
+ 
   return (
-    <div className="border rounded-lg px-2 m-2">
-      <div className='text-base font-semibold py-2'>
-         {title}
-      </div>
-      <div className={'flex gap-4 justify-between py-2 text-gray-700'}>
-        <div>{id}</div>
-        <div>El</div>
-        <div>{points}</div>
-      </div>
-    </div>
+    <>
+      <TaskCard title={title} id={id} points={points}></TaskCard>
+      <TaskCard title={'Competitor analysis'} id={'BUS-2'} points={3}></TaskCard>
+      <TaskCard title={'Develop Business Strategy'} id={'BUS-3'} points={8}></TaskCard>
+      <TaskCard title={'Develop Marketing Strategy'} id={'BUS-4'} points={5}></TaskCard>
+    </>
   )
 }
 
