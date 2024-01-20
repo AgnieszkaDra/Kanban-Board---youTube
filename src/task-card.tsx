b5b4fafff39bc5914cb881acea2332e6b64230a6
Agnieszka Draganczyk <Agnieszka.127@interia.pl>
2024-01-20 14:05:54 +0100

    types

--- src/task-card.tsx
+++ src/task-card.tsx
@@ -1,12 +1,17 @@
-const TaskCard = ({title, id, points}) => {
+const TaskCard = ({task}: {
+  task: Task
+ }) => {
+  const num = 1
+  const num2 = 2
+  const num3 = num + num2
     return <div className="border rounded-lg px-2 m-2">
       <div className='text-base font-semibold py-2'>
-         {title}
+         {task.title}!!! {num3}
       </div>
       <div className={'flex gap-4 justify-between py-2 text-gray-700'}>
-        <div>{id}</div>
+        <div>{task.id}</div>
         <div>El</div>
-        <div>{points}</div>
+        <div>{task.points}</div>
       </div>
     </div>
   } 
