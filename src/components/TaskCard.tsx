f6b5619786c300c5ede093000c9505946336183c
Agnieszka Draganczyk <Agnieszka.127@interia.pl>
2024-01-21 18:07:29 +0100

    priority icons

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -1,5 +1,59 @@
 import { Task } from "../utils/data-tasks"
 
+const highPriorityIcon = <svg
+xmlns="http://www.w3.org/2000/svg"
+fill="none"
+viewBox="0 0 24 24"
+stroke="currentColor"
+width="24"
+height="24"
+>
+<g>
+  <path
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    stroke-width="2"
+    d="M5 10l7-7m0 0l7 7m-7-7v18"
+  ></path>
+</g>
+</svg>
+
+const lowPriorityIcon = <svg
+xmlns="http://www.w3.org/2000/svg"
+fill="none"
+viewBox="0 0 24 24"
+stroke="currentColor"
+width="24"
+height="24"
+>
+<g>
+  <path
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    stroke-width="2"
+    d="M19 14l-7 7m0 0l-7-7m7 7V3"
+  ></path>
+</g>
+</svg>
+
+const equalPriorityIcon = <svg
+xmlns="http://www.w3.org/2000/svg"
+fill="none"
+viewBox="0 0 24 24"
+stroke="currentColor"
+width="24"
+height="24"
+>
+<g>
+  <path
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    stroke-width="2"
+    d="M4 6h16M4 12h16m-7 6h7"
+  ></path>
+</g>
+</svg>
+
 const TaskCard = ({task}: {
   task: Task
  }) => {
@@ -11,8 +65,13 @@ const TaskCard = ({task}: {
          {task.title}!!! {num3}
       </div>
       <div className={'flex gap-4 justify-between py-2 text-gray-500 text-sm'}>
-        <div>{task.id}</div>
-        <div>El</div>
+        <div className={'flex gap-2'}>
+          <div>{task.id}</div>
+          <div>{task.priority}</div>
+          {task.priority === 'high' ? <div>{highPriorityIcon}</div> : null}
+          {task.priority === 'medium' && <div>{equalPriorityIcon}</div>}
+          {task.priority === 'low' && <div>{lowPriorityIcon}</div>}
+        </div>
         <div>{task.points}</div>
       </div>
     </div>
--- src/utils/data-tasks.tsx
+++ src/utils/data-tasks.tsx
@@ -1,46 +1,55 @@
 export type Status = 'todo' | 'in-progress' | 'done'
+export type Priority = 'low' | 'medium' | 'high'
 export type Task = {
     title: string,
     id: string, 
-    status: Status
+    status: Status,
+    priority: Priority,
     points?: number
   }
 
 export const statutes: Status[] = ['todo', 'in-progress', 'done']
+export const priorities: Priority[] = ['low', 'medium', 'high']
 
 export const tasks: Task[] =  [
     {  
       title: 'Do Market Research',
       id: 'BUS-1',
       status: 'todo',
+      priority: 'low',
       points: 5,
     },
     {
       title: 'Competitor analysis',
       id: 'BUS-2',
+      priority: 'medium',
       status: 'done'
     },
     {
       title: 'Develop Business Strategy',
       id: 'BUS-3',
+      priority: 'high',
       status: 'in-progress',
       points: 8,
     },
     {
       title: 'Develop Marketing Strategy',
       id: 'BUS-4',
+      priority: 'high',
       status: 'todo',
       points: 5,
     },
     {
       title: 'Develop marketing plan',
       id: 'BUS-5',
+      priority: 'medium',
       status: 'todo',
       points: 1,
     },
     {
       title: 'Implement marketing plan',
       id: 'BUS-6',
+      priority: 'low',
       status: 'todo',
       points: 4,
     }
